58e95131a9c6606f60ace01d99a73210089e5b59
Ian Wold <ilwold@hotmail.com>
2022-08-13 13:06:03 -0500

    Add project files.

---
+++ Metalsharp.LiquidTemplates/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Metalsharp.LiquidTemplates;
+
+public static class Extensions
+{
+	public static MetalsharpProject UseLiquidTemplates(this MetalsharpProject project, string templateDirectory, bool loadFromFilesystem = true) =>
+		project.Use(new LiquidTemplates(templateDirectory, loadFromFilesystem));
+}
---
+++ Metalsharp.LiquidTemplates/LiquidTemplates.cs
@@ -0,0 +1,53 @@
+﻿using Fluid;
+
+namespace Metalsharp.LiquidTemplates;
+
+public class LiquidTemplates : IMetalsharpPlugin
+{
+	const string _defaultTemplateDirectory = "liquid-templates";
+
+	readonly string _templateDirectory;
+	readonly bool _loadFromFilesystem;
+
+	public LiquidTemplates(string templateDirectory, bool loadFromFilesystem = true)
+	{
+		_templateDirectory = templateDirectory;
+		_loadFromFilesystem = loadFromFilesystem;
+	}
+
+	string VirtualTemplateDirectory =>
+		_loadFromFilesystem
+		? _defaultTemplateDirectory
+		: _templateDirectory;
+
+	public void Execute(MetalsharpProject project)
+	{
+		if (_loadFromFilesystem)
+		{
+			project.AddInput(_templateDirectory, "fluid-templates");
+		}
+		IFluidTemplate? layout = null;
+
+		if (project.InputFiles.SingleOrDefault(f => f.FilePath == $"{VirtualTemplateDirectory}\\layout.liquid") is MetalsharpFile layoutFile)
+		{
+			new FluidParser().TryParse(layoutFile.Text, out layout);
+		}
+
+		foreach (var output in project.OutputFiles)
+		{
+			if (output.Metadata.TryGetValue("template", out object? templateFileObject)
+				&& templateFileObject is string templateFilePath
+				&& project.InputFiles.SingleOrDefault(f => f.FilePath == templateFilePath) is MetalsharpFile templateFile
+				&& new FluidParser().TryParse(templateFile.Text, out var template)
+			)
+			{
+				output.Text = template.Render(new TemplateContext(new { content = output.Text }));
+			}
+
+			if (layout is not null)
+			{
+				output.Text = layout.Render(new TemplateContext(new { content = output.Text }));
+			}
+		}
+	}
+}
